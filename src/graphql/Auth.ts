6cfa2e314851db607b39f7d0cf002482ba5dabd7
CRBroughton <crbroughton@posteo.uk>
2022-01-09 10:25:36 +0000

    created login definition

--- src/graphql/Auth.ts
+++ src/graphql/Auth.ts
@@ -16,6 +16,39 @@ export const AuthPayload = objectType({
 export const AuthMutation = extendType({
   type: 'Mutation',
   definition(t) {
+    t.nonNull.field('login', {
+      type: 'AuthPayload',
+      args: {
+        email: nonNull(stringArg()),
+        password: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, context) {
+        // 1
+        const user = await context.prisma.user.findUnique({
+          where: { email: args.email },
+        })
+        if (!user)
+          throw new Error('No such user found')
+
+        // 2
+        const valid = await bcrypt.compare(
+          args.password,
+          user.password,
+        )
+        if (!valid)
+          throw new Error('Invalid password')
+
+        // 3
+        const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+        // 4
+        return {
+          token,
+          user,
+        }
+      },
+    })
+
     t.nonNull.field('signup', { // 1
       type: 'AuthPayload',
       args: {
@@ -23,7 +56,7 @@ export const AuthMutation = extendType({
         password: nonNull(stringArg()),
         name: nonNull(stringArg()),
       },
-      async resolve(parent, args, context) {
+      async resolve(_parent, args, context) {
         const { email, name } = args
         // 2
         const password = await bcrypt.hash(args.password, 10)
