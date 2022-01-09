42f12e13f005bf1ee82acc925c193e7914af9acc
CRBroughton <crbroughton@posteo.uk>
2022-01-09 09:35:01 +0000

    created main func for prisma

---
+++ src/script.ts
@@ -0,0 +1,22 @@
+// 1
+import { PrismaClient } from '@prisma/client'
+
+// 2
+const prisma = new PrismaClient()
+
+// 3
+async function main() {
+  const allLinks = await prisma.link.findMany()
+  // eslint-disable-next-line no-console
+  console.log(allLinks)
+}
+
+// 4
+main()
+  .catch((e) => {
+    throw e
+  })
+// 5
+  .finally(async() => {
+    await prisma.$disconnect()
+  })
