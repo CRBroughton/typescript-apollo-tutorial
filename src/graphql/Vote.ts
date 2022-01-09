444fca1e19a1a0966ba15018c6393bdb99d65430
CRBroughton <crbroughton@posteo.uk>
2022-01-09 11:43:06 +0000

    create Vote Type and Mutation

---
+++ src/graphql/Vote.ts
@@ -0,0 +1,21 @@
+import { extendType, intArg, nonNull, objectType } from 'nexus'
+
+export const Vote = objectType({ // 1
+  name: 'Vote',
+  definition(t) {
+    t.nonNull.field('link', { type: 'Link' })
+    t.nonNull.field('user', { type: 'User' })
+  },
+})
+
+export const VoteMutation = extendType({ // 2
+  type: 'Mutation',
+  definition(t) {
+    t.field('vote', {
+      type: 'Vote',
+      args: {
+        linkId: nonNull(intArg()),
+      },
+    })
+  },
+})
