1054837189ca9d4df29f5678ecd209ae61bd644b
CRBroughton <crbroughton@posteo.uk>
2022-01-09 10:29:21 +0000

    updated context with decodeAuthHeader

--- src/context.ts
+++ src/context.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from '@prisma/client'
+import type { Request } from 'express'
+import { AuthTokenPayload, decodeAuthHeader } from './utils/auth'
+
 export const prisma = new PrismaClient()
 
 export interface Context { // 1
   prisma: PrismaClient
+  userId?: number
 }
 
-export const context: Context = { // 2
-  prisma,
+export const context = ({ req }: { req: Request }): Context => { // 2
+  const token
+      = req && req.headers.authorization
+        ? decodeAuthHeader(req.headers.authorization)
+        : null
+
+  return {
+    prisma,
+    userId: token?.userId,
+  }
 }
